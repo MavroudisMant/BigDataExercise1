98252ee858ae738af429f62f6aaa2669f6d70a5f
Mavroudis Mantonitsas <m.mantonitsas@gmail.com>
2021-12-11 12:39:51 +0200

    Still trying

--- src/main/java/FileAccessCount.java
+++ src/main/java/FileAccessCount.java
@@ -1,4 +1,6 @@
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
@@ -11,7 +13,7 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class FileAccessCount {
-	public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable>{
+	public static class TokenizerMapper extends Mapper<Object, Text, Text, Text>{
 		
 		private final static IntWritable one = new IntWritable(1);
 		private Text word = new Text();
@@ -29,39 +31,56 @@ public class FileAccessCount {
 			if(file.charAt(0) == '.') {
 				finalName = accessionNum + file;
 			}
+		
+			word.set(ip + "," + finalName);
 			
-			word.set(ip + "," + finalName + "," + date);
-			context.write(word, one);
-					
+			context.write(word, new Text(date));
+								
+			
 		}
 	}
 	
-	public static class IntSumReducer extends Reducer<Text,IntWritable,Text,IntWritable>{
+	public static class IntSumReducer extends Reducer<Text,IntWritable,Text,Text>{
 		private IntWritable result = new IntWritable();
 		
-		public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+			ArrayList<Text> dates = new ArrayList<>();
 			int sum = 0;
-			for (IntWritable val : values) {
-			 sum += val.get();
+			for (Text val : values) {
+				int s = 0;
+				for(Text date : dates) {
+					if(date.equals(val)) {
+						s++;
+					}
+				}
+				if(s>0)
+				{
+					sum++;
+				}
 			}
-			result.set(sum);
 			
-			IntWritable one = new IntWritable(1);
-			
-			if(result.compareTo(one) == 1)
-				context.write(key, result);
+			if(sum>1)
+			{
+				context.write(key, new Text(" "));
+			}
+//			result.set(sum);
+//			
+//			IntWritable one = new IntWritable(1);
+//			
+//			if(result.compareTo(one) == 1)
+				
 		}
 	}
 	
 	public static void main(String[] args) throws Exception {
 	    Configuration conf = new Configuration();
-	    Job job = Job.getInstance(conf, "word count");
+	    Job job = Job.getInstance(conf, "access count");
 	    job.setJarByClass(FileAccessCount.class);
 	    job.setMapperClass(TokenizerMapper.class);
 	    job.setCombinerClass(IntSumReducer.class);
 	    job.setReducerClass(IntSumReducer.class);
 	    job.setOutputKeyClass(Text.class);
-	    job.setOutputValueClass(IntWritable.class);
+	    job.setOutputValueClass(Text.class);
 	    FileInputFormat.addInputPath(job, new Path(args[0]));
 	    FileOutputFormat.setOutputPath(job, new Path(args[1]));
 	    System.exit(job.waitForCompletion(true) ? 0 : 1);
